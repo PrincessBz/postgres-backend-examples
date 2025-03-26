2f3cc6acb9a1ba2f14147e28c4ad3881ec405c1f
PrincessBz <162399259+PrincessBz@users.noreply.github.com>
2025-03-26 03:23:02 -0800

    My work
    Class work

--- item-cli-app/index.js
+++ item-cli-app/index.js
@@ -1,33 +1,75 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "1964",
+  port: 5432,
+});
+
 /**
  * Creates the database table, if it does not already exist.
  */
 async function createTable() {
-
-};
+  const query = `
+    CREATE TABLE IF NOT EXISTS items (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL
+    );
+  `;
+  try {
+    await pool.query(query);
+    console.log("Table created or already exists");
+  } catch (error) {
+    console.error("Error creating table: ", error.stack);
+  }
+}
 
 /**
  * Inserts a new item into the table
- * 
+ *
  * @param {string} itemName Name of the item being added
  */
 async function insertItem(itemName) {
+  const query = {
+    text: "INSERT INTO items (name) VALUES  ($1)",
+    values: [itemName],
+  };
 
-};
+  try {
+    await pool.query(query, [itemName]);
+    console.log(`Item  ${itemName} inserted successfully`);
+  } catch (error) {
+    console.error("Error inserting item: ", error.stack);
+  }
+}
 
 /**
  * Prints all items in the database to the console
  */
 async function displayItems() {
+  const query = `
+    SELECT * FROM items;
+  `;
+  const result = await pool.query(query);
 
-};
+  if (result.rows.length === 0) {
+    console.log("No items");
+  }
+
+  result.rows.forEach((row) => {
+    console.log(`${row.id}: ${row.name}`);
+  });
+}
 
 /**
  * Prints a help message to the console
  */
 function printHelp() {
-  console.log('Usage:');
-  console.log('  insert <item_name> - Insert an item');
-  console.log('  show - Show all items');
+  console.log("Usage:");
+  console.log("  insert <item_name> - Insert an item");
+  console.log("  show - Show all items");
 }
 
 /**
@@ -38,7 +80,7 @@ async function runCLI() {
 
   const args = process.argv.slice(2);
   switch (args[0]) {
-    case 'insert':
+    case "insert":
       if (!args[1]) {
         printHelp();
         return;
@@ -46,13 +88,15 @@ async function runCLI() {
 
       await insertItem(args[1]);
       break;
-    case 'show':
+    case "show":
       await displayItems();
       break;
     default:
       printHelp();
       break;
   }
-};
+  // Close the pool when we're done
+  pool.end();
+}
 
 runCLI();
--- todo-list-app/index.js
+++ todo-list-app/index.js
@@ -4,8 +4,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres', //This _should_ be your username, as it's the default one Postgres uses
   host: 'localhost',
-  database: 'keyin_test', //This should be changed to reflect your actual database
-  password: 'Keyin123', //This should be changed to reflect the password you used when setting up Postgres
+  database: 'postgres', //This should be changed to reflect your actual database
+  password: '1964', //This should be changed to reflect the password you used when setting up Postgres
   port: 5432,
 });
 
@@ -13,6 +13,14 @@ const pool = new Pool({
  * Creates the to do list table, if it does not already exist
  */
 async function createTable() {
+  const query = `
+    CREATE TABLE IF NOT EXISTS todos (
+      id SERIAL PRIMARY KEY,
+      task TEXT NOT NULL,
+      completed BOOLEAN DEFAULT FALSE
+    );
+  `;
+    await pool.query(query);
 
 };
 
@@ -22,14 +30,27 @@ async function createTable() {
  * @param {string} task - The item to add
  */
 async function addTodo(task) {
+  const query = 
+   'INSERT INTO todos (task) VALUES ($1) RETURNING *;';
+  const response = await pool.query(query, [task]);
+  console.log(`Added todo: ${response.rows[0].task}`);
+  };
 
-};
 
 /**
  * Prints all todo list items to the console
  */
 async function showTodos() {
+  const query = 'SELECT * FROM todos;';
+  const response = await pool.query(query);
+  if (response.rows.length < 1 ) {
+    console.log("To-do list is empty");
+    return;
+  }
 
+  response.rows.forEach((todo) => {
+    console.log(`${todo.id}: ${todo.task} [${todo.completed ? 'Completed' : 'Not Completed'}]`);
+  });
 };
 
 /**
@@ -38,6 +59,15 @@ async function showTodos() {
  * @param {number} id - The ID of the todo list item
  */
 async function completeTodo(id) {
+  const query = 'UPDATE todos SET completed = TRUE WHERE id = $1 RETURNING *;';
+  const response = await pool.query(query, [id]);
+  if (response.rowCount < 0) {
+    console.log(`Marked item ${id} as completed.`);
+  }
+  else {
+    console.log(`No item was found with id ${id}.`);
+  }
+  
 
 };
 
@@ -47,6 +77,14 @@ async function completeTodo(id) {
  * @param {number} id - The ID of the todo list item
  */
 async function deleteTodo(id) {
+  const query = 'DELETE FROM todos WHERE id = $1 RETURNING *;';
+  const response = await pool.query(query, [id]);
+  if (response.rowCount < 0) {
+    console.log(`Deleted item id ${id} from the list.`);
+  }
+  else {
+    console.log(`No item was found with id ${id}.`);
+  }
 
 };
 
@@ -54,6 +92,9 @@ async function deleteTodo(id) {
  * Removes all todo list items that have been completed
  */
 async function clearCompleted() {
+  const query = 'DELETE FROM todos WHERE completed = TRUE;';
+  const response = await pool.query(query);
+  console.log(`Cleared ${response.rowCount} completed items from the list.`);
 
 };
 
